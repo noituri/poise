dafc26068c883eae88fdb633cecbd0ad3a719ea4
kangalioo <kangalioo@users.noreply.github.com>
2021-09-11 11:24:33 +0000

    deploy: 378fea009e3624b38d85d8127ecc52e59360e071

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["poise"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"poise/struct.Wrapper.html\" title=\"struct poise::Wrapper\">Wrapper</a>&lt;T&gt;","synthetic":false,"types":["poise::prefix::argument::wrapper::Wrapper"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
